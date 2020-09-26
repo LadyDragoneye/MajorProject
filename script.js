0c175ed3dbb676e6c31d48ce7b07145cd405b1c7
Glitch (hello-webpage) <none>
2020-09-26 04:47:30 +0000

    💫👩‍👩‍👦 Checkpoint ./style.css:17667160/856 ./index.html:17667160/33 ./script.js:17667160/112

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ var age = prompt("Please input your age: ");
 var cost;
 
 if (age < 5){
-  document.getElementById("cost").innerHTML = "Entry is free!";
+  document.getElementById("cost").innerHTML = "Under age 5, entry is free!";
 }
 
 if (age >= 5 && age <=12 ){
-  document.getElementById("cost").innerHTML = "Ticket is $5.00";
+  document.getElementById("cost").innerHTML = "Tickets cost $5.00 for ages 5 - 12";
 }
 
 if (age > 12){
-  document.getElementById("cost").innerHTML = "Ticket is $9.00";
+  document.getElementById("cost").innerHTML = "Tickets cost $9.00 for ages over 12";
 }
 
 
