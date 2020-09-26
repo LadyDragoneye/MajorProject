695b918099d72cce358f2d4ecb5a5f0de03885e9
Glitch (hello-webpage) <none>
2020-09-26 05:27:31 +0000

    🚐🔋 Checkpoint ./script.js:17667160/141

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
 var age = prompt("Please input your age: ");
 var cost;
 
